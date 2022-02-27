acf19f58b617592a9283a44cab51e9b065271fed
ManuelEdesa <war4zone@gmail.com>
2022-02-27 13:00:11 +0100

    1 file modified

--- index.js
+++ index.js
@@ -11,15 +11,25 @@ class App extends Component {
     };
     this.showDetails = this.showDetails.bind(this);
     this.searchTerm = this.search.bind(this);
+    this.clearInput = this.clearInput.bind(this);
   }
-
+  clearInput() {
+    this.setState({searchTerm: '' });
+    // ReactDOM.findDOMNode(this.refs.myInput).focus();
+    this.refs.myInput.focus();
+  }
   componentWillMount() {
     fetch(`https://api.imgflip.com/get_memes`).then((res) => res.json()).then((data) => {
       this.setState({users: data.data.memes})
      
     })
   }
+  onClickAñadir() {
+    console.log(e.target.value)
+    this.setState({searchTerm: e.target.value})
 
+  }
+  
   search(e) {
     console.log(e.target.value)
     this.setState({searchTerm: e.target.value})
@@ -39,7 +49,9 @@ class App extends Component {
       <header>
         <h1>Buscador de Memes </h1>
       </header>
-      <input class="search-box" onKeyUp={(e) => this.searchTerm(e)}   type="text"></input>
+     
+      <input class="search-box" onKeyUp={(e) => this.searchTerm(e)} ref="myInput"  type="text"></input>
+      <button onClick={this.clearInput}>CLEAR</button>
         <ul class="collapse-able">
         {this.state.users.filter(user => {
           return user.name.toLowerCase().indexOf(this.state.searchTerm) > -1;
